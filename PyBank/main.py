d6f1a97f608ad9d752a343eda6b329fed427f31f
MrScottGregory <segbrt@gmail.com>
2019-05-11 21:24:58 -0400

    final solutions added

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,57 +11,90 @@ with open(budget_data, newline='') as csvfile:
 
     # set variable to hold row count and start at 0
     rows_count = 0
-    # set variable to hold net_profit so we can sum it by row
+    # set variable to hold row profit total
     net_profit = 0
     # establish a list to hold all the values in the month column
     month_list = []
     # establish a list to hold all the values in the profit column
     profit_list = []
+    # establish a list to hold monthly profit/loss changes
+    monthly_revenue_changes = []
 
+# ---------------------------------------------------------------------------
 # TASK #1 calculate total number of months included in the dataset
+# ---------------------------------------------------------------------------
+
     # loop through each row
     for row in csvreader:
         # add 1 for each row in loop (total # of row = total months)
         rows_count = rows_count + 1
 
+# ---------------------------------------------------------------------------
 # TASK #2 calculate net total amount of "Profit/Losses" over the entire period
+# ---------------------------------------------------------------------------
+
         # sum column 2
         net_profit = net_profit + int(row[1])
 
+# ---------------------------------------------------------------------------
 # TASK # 3 calculate average of the changes in "Profit/Losses" over the entire period
-        # casting as float allows us to have points after decimal (more accurate for avg)
-        avg_profit = float(net_profit / rows_count)  
+# ---------------------------------------------------------------------------
 
-# TASK # 4 identify the greatest increase in profits (date and amount) over the entire period
-        # append all of the values in profit column to make an indexed list
-        profit_list.append(int(row[1]))
+        # append all of the values in "Profit/Losses" column to make an indexed list
+        profit_list.append(row[1])
         # append all of the values in month column to make an indexed list
         month_list.append(row[0])
-        # use max function to search for max value in profit list
-        max_profit = max(profit_list)
-        # use index function to grab the index number for the max value
-        max_index = profit_list.index(max_profit)
-        # search for the month that corresponds to that same index number in the month list
-        max_month = month_list[max_index]
+    # >> test point >> print(profit_list)
+    # >> test point >> print(month_list)
+    
+    # start a new loop that runs through the range of profits starting with SECOND row (index 1)
+    # the end of the range should be the length of the new list, however long it gets
+    for i in range(1,len(profit_list)):
+        # calculate difference in profit values between rows, and populate monthly rev change list with those values
+        monthly_revenue_changes.append(int(profit_list[i]) - int(profit_list[i-1]))   
+        # calculate mean by summing the list and then dividing by it's length
+        avg_rev_change = sum(monthly_revenue_changes)/len(monthly_revenue_changes)
+    # >> test point >> print(avg_rev_change)
+
+# ---------------------------------------------------------------------------
+# TASK # 4 identify the greatest increase in profits (date and amount) over the entire period
+# ---------------------------------------------------------------------------
+
+        # use max() to identify biggest value in monthly rev change list
+        max_rev_change = max(monthly_revenue_changes)
+    # >> test point >> print(max_rev_change)
+        # use index function to identify the index number of the max value
+        max_index = monthly_revenue_changes.index(max_rev_change)
+        # because profit loop started at index 1, it has 1 fewer indexes than the list of months
+        # to identify max month, use max index from profit list, but add 1
+        max_month = month_list[max_index + 1]
+    # >> test point >> print(max_month)
+
+# ---------------------------------------------------------------------------
+# TASK # 5 identify the greatest decrease in losses (date and amount) over the entire period       
+# ---------------------------------------------------------------------------
        
+        # now do all of the same steps for the min values
+        min_rev_change = min(monthly_revenue_changes)
+        min_index = monthly_revenue_changes.index(min_rev_change)
+        min_month = month_list[min_index + 1]
 
-# TASK # 5 identify the greatest decrease in losses (date and amount) over the entire period
-        min_profit = min(profit_list)
-        min_index = profit_list.index(min_profit)
-        min_month = month_list[min_index]
-
-# TASK #6 print analysis summary to terminal    
+# ---------------------------------------------------------------------------
+# TASK #6 print analysis summary to terminal  
+# ---------------------------------------------------------------------------
+  
     print("Financial Analysis")
     print("-------------------------------------------------------------------")
     print(f'Total Months: {rows_count}')
     print(f'Total: ${net_profit}')
     # the :.2F code tells python to limit the number (a float, see above) to just 2 decimal places
-    print(f'Average Change: ${avg_profit:.2F}')
-    print(f'Greatest Increase in Profits: {max_month} (${max_profit})')
-    print(f'Greatest Decrease in Profits: {min_month} (${min_profit})')
+    print(f'Average Change: ${avg_rev_change:.2F}')
+    print(f'Greatest Increase in Profits: {max_month} (${max_rev_change})')
+    print(f'Greatest Decrease in Profits: {min_month} (${min_rev_change})')
 
-
+# ---------------------------------------------------------------------------
 # TASK #7 export results to a text file
+# ---------------------------------------------------------------------------
 
 file = open("PyBankResults.txt", "w+")
 
@@ -69,6 +102,6 @@ file.write("Financial Analysis\n")
 file.write("-------------------------------------------------------------------\n")
 file.write(f'Total Months: {rows_count}\n')
 file.write(f'Total: ${net_profit}\n')
-file.write(f'Average Change: ${avg_profit:.2F}\n')
-file.write(f'Greatest Increase in Profits: {max_month} (${max_profit})\n')
-file.write(f'Greatest Decrease in Proftis: {min_month} (${min_profit})')
+file.write(f'Average Change: ${avg_rev_change:.2F}\n')
+file.write(f'Greatest Increase in Profits: {max_month} (${max_rev_change})\n')
+file.write(f'Greatest Decrease in Profits: {min_month} (${min_rev_change})')
